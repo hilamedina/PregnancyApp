4ea593a1710ff1c7a34f2673c7117b67e7b26f88
hilamedina <hm18986@gmail.com>
2022-02-05 02:25:33 +0200

    btn

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 // import LogingPage from './components/LoginPage/LoginPage';
 import SwipeableTextMobileStepper from './components/carousel/carousel';
 // import DatePicker from './components/datepicker/datePicker';
+import Btn from './components/butoonComponent/btn';
 
 function App() {
   return (
     <div className="App">
-      <SwipeableTextMobileStepper />
+      {/* <SwipeableTextMobileStepper /> */}
       {/* <LogingPage /> */}
       {/* <DatePicker /> */}
+      <Btn />
     </div>
   );
 }
